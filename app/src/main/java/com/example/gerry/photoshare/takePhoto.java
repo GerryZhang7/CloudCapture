e6bd042d24cfe3848865108cfc30e6ee8e9eb914
GerryZhang7 <Gerryzhang123@gmail.com>
2018-12-27 23:51:16 -0800

    Added photo taking ability to takePhoto activity

--- app/src/main/java/com/example/gerry/photoshare/takePhoto.java
+++ app/src/main/java/com/example/gerry/photoshare/takePhoto.java
@@ -1,15 +1,25 @@
 package com.example.gerry.photoshare;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class takePhoto extends AppCompatActivity {
 
@@ -104,6 +114,62 @@ public class takePhoto extends AppCompatActivity {
             }
         }
     }
+
+    //This function makes sure the camera is disabled when the device is shut down or closed.
+    public class CameraActivity extends Activity {
+
+        private Camera mCamera;
+        private CameraPreview mPreview;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+    }
+
+    private PictureCallback mPicture = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    // Add a listener to the Capture button
+    Button captureButton = (Button) findViewById(R.id.button_capture);
+    captureButton.setOnClickListener(
+        new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // get an image from the camera
+            mCamera.takePicture(null, null, mPicture);
+        }
+    }
+);
 
     //ADD PREVIEW IN XML LAYOUT TOMORROW
    /* private boolean safeCameraOpen(int id) {
