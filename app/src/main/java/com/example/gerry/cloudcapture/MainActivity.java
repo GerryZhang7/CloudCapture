c17d2112ac5ca02291f2ee5c8ef9011fbc8b2354
GerryZhang7 <Gerryzhang123@gmail.com>
2019-01-11 02:10:06 -0500

    Fixed minor issues

--- app/src/main/java/com/example/gerry/cloudcapture/MainActivity.java
+++ app/src/main/java/com/example/gerry/cloudcapture/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.gerry.cloudcapture;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
     ImageButton photoButton, recordButton, playVideoButton;
     private int ACTIVITY_START_CAMERA_APP = 0;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    String mCurrentPhotoPath;
+    private String mCurrentPhotoPath = "";
 
+    Uri photoURI = null;
+    File photoFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,19 +85,17 @@ public class MainActivity extends AppCompatActivity {
     }
 */
 
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp +"_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
 
+    /*
+    // IF NEXT IMPLEMENTATION DOES NOT WORK UNCOMMENT THIS FUNCTION
     static final int REQUEST_TAKE_PHOTO = 1;
     //Change this function to public if it doesn't work
     public void dispatchTakePictureIntent(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -110,13 +117,73 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
+    }*/
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent()
+    {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        else
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                try {
+
+                    photoFile = createImageFile();
+                    displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                    Log.i("Mayank",photoFile.getAbsolutePath());
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        photoURI = FileProvider.getUriForFile(this,
+                                "com.example.gerry.cloudcapture.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                    }
+                } catch (Exception ex) {
+                    // Error occurred while creating the File
+                    displayMessage(getBaseContext(),ex.getMessage().toString());
+                }
+
+
+            }else
+            {
+                displayMessage(getBaseContext(),"Nullll");
+            }
+        }
+
+
+
     }
 
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp +"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            mImageView.setImageBitmap(myBitmap);
+        }
+        else
+        {
+            displayMessage(getBaseContext(),"Request cancelled or something went wrong.");
+        }
+       /* if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
@@ -125,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
             //These lines control video playback
             Uri videoUri = data.getData();
             mVideoPlayBack.setVideoURI(videoUri);
-        }
+        }*/
     }
 
     private void galleryAddPic() {
@@ -134,6 +201,25 @@ public class MainActivity extends AppCompatActivity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            }
+            else {
+                displayMessage(getBaseContext(), "Please enable the camera to use this app");
+            }
+        }
+
+    }
+
+    private void displayMessage(Context context, String message)
+    {
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
     }
 
 }
